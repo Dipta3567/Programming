68c33ef6bb82c5b9f0f563aed3733b2d0851b39b
Dipta3567 <diptadhor2002@gmail.com>
2024-10-17 00:33:40 +0600

    updated on 17/10/2024

---
+++ Coding/Software Development/Django/Django Codes/.room2/project_2/first_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(req):
+     #return HttpResponse('This is Home page')
+     return render(req, 'first_app/home.html')
---
+++ Coding/Software Development/Django/Django Codes/simple_page_navigarion/navigation/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+def about (req):
+    #return render(req,'about.html')
+    pass
+
+def contact (req):
+    #return render(req,'contact.html')
+    pass
