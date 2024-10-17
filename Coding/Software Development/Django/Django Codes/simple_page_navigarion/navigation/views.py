6638572a8de364904554d325d8f732b4a515eb3e
Dipta3567 <diptadhor2002@gmail.com>
2024-10-17 23:01:44 +0600

    Render templates and page navigation. updated on 17/10/2024

--- Coding/Software Development/Django/Django Codes/simple_page_navigarion/navigation/views.py
+++ Coding/Software Development/Django/Django Codes/simple_page_navigarion/navigation/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def about (req):
-    #return render(req,'about.html')
-    pass
+    return render(req,'navigation/about.html')
+    
 
 def contact (req):
-    #return render(req,'contact.html')
-    pass
+    return render(req,'navigation/contact.html')
+    
